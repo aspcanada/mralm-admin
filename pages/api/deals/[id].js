c234e850874047494d9eed9e3939162f8039c09c
Alain St. Pierre <alain@aspcanada.com>
2023-01-28 22:11:50 -0800

    use json-server

--- pages/api/deals/[id].js
+++ pages/api/deals/[id].js
@@ -1,12 +1,12 @@
-import { data } from "./db"
-
 export default async function handler(req, res) {
-  
   const { id } = req.query;
+
+  const resp = await fetch("http://localhost:8081/deals/"+id);
+  const data = await resp.json();
 
   try {
     // res.status(200).json(singleDeal);
-    res.status(200).json(data[id]);
+    res.status(200).json(data);
   } catch (err) {
     console.log(err)
     // alert("Data is not fetch!!! Please check console!!!");
--- pages/api/deals/index.js
+++ pages/api/deals/index.js
@@ -1,6 +1,8 @@
-import { data } from "./db"
+export default async function handler(req, res) {
 
-export default function handler(req, res) {
+  const resp = await fetch("http://localhost:8081/deals");
+  const data = await resp.json();
+
   try {
     res.status(200).json(data);
   } catch (err) {
