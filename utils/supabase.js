9f81934049f59ef089fdfd32d8376c67740771ec
Alain St. Pierre <alain@aspcanada.com>
2023-02-08 14:12:59 -0800

    add supabase

---
+++ utils/supabase.js
@@ -0,0 +1,12 @@
+import { createClient } from "@supabase/supabase-js"
+
+const getSupabase = () => {
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_KEY
+  )
+
+  return supabase
+}
+
+export { getSupabase }
